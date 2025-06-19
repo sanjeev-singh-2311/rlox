213e8e43d87011ad7767ad964c21682c111c5b23
Sanjeev Singh <sanjeev.2311.singh@gmail.com>
2025-06-19 20:56:46 +0530

    [fix] the error function now properly exits with code 64

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,10 @@
+use std::process::exit;
+
 pub fn show_error(line: u32, message: String) {
     report(line, "", message)
 }
 
 fn report(line: u32, location: &str, message: String) {
     eprintln!("[line \"{line}\"] Error {location} : {message}");
+    exit(64)
 }
