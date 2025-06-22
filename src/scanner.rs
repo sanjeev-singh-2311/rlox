96a690db81451fb3b95f622bd63169281efa8290
Sanjeev Singh <sanjeev.2311.singh@gmail.com>
2025-06-22 16:38:08 +0530

    [refactor] sorted scanner methods alphabetically

--- src/scanner.rs
+++ src/scanner.rs
@@ -23,6 +23,86 @@ impl Scanner {
         }
     }
 
+    fn add_token(&mut self, tok_type: TokenType, literal: Option<Box<dyn std::any::Any>>) {
+        match literal {
+            Some(lit) => self.add_token_in_vec(tok_type, lit),
+            None => self.add_token_in_vec(tok_type, Box::<Option<i8>>::new(None)),
+        }
+    }
+
+    fn add_token_in_vec(&mut self, token_type: TokenType, literal: Box<dyn std::any::Any>) {
+        let substr = self.source[self.start..self.current].to_owned();
+        self.tokens
+            .push(Token::new(token_type, substr, literal, self.line));
+    }
+
+    fn advance(&mut self) -> char {
+        let local_current = self.current;
+        self.current += 1;
+        self.source_iter[local_current]
+    }
+
+    fn char_is_digit(&self, c: char) -> bool {
+        c >= '0' && c <= '9'
+    }
+
+    fn check_curr(&mut self, expected: char) -> bool {
+        if self.lookahead() == expected {
+            self.advance();
+            return true;
+        }
+        false
+    }
+
+    fn lex_number(&mut self) {
+        while self.char_is_digit(self.lookahead()) {
+            self.advance();
+        }
+        if self.lookahead() == '.' && self.char_is_digit(self.lookahead_2()) {
+            self.advance();
+            while self.char_is_digit(self.lookahead()) {
+                self.advance();
+            }
+        }
+        let literal = self.source[self.start..self.current]
+            .to_owned()
+            .parse::<f32>();
+        match literal {
+            Ok(n) => self.add_token(TokenType::NUMBER, Some(Box::new(literal))),
+            Err(_) => show_error(self.line, "Invalid number literal somehow".to_owned()),
+        }
+    }
+
+    fn lex_string(&mut self) {
+        while !self.is_at_end() && self.lookahead() != '"' {
+            if self.lookahead() == '\n' {
+                self.line += 1;
+            }
+            self.advance();
+        }
+        if self.is_at_end() {
+            show_error(self.line, "Unterminated string".to_owned());
+        }
+        self.advance();
+
+        let value = self.source[self.start + 1..self.current - 1].to_owned();
+        self.add_token(TokenType::STRING, Some(Box::new(value)));
+    }
+
+    fn lookahead(&self) -> char {
+        if self.is_at_end() {
+            return '\0';
+        }
+        self.source_iter[self.current]
+    }
+
+    fn lookahead_2(&self) -> char {
+        if self.current + 1 >= self.source_iter.len() {
+            return '\0';
+        }
+        self.source_iter[self.current + 1]
+    }
+
     fn is_at_end(&self) -> bool {
         if self.current == self.source_iter.len() {
             return true;
@@ -83,85 +163,5 @@ impl Scanner {
             }
         }
         ()
-    }
-
-    fn add_token(&mut self, tok_type: TokenType, literal: Option<Box<dyn std::any::Any>>) {
-        match literal {
-            Some(lit) => self.add_token_in_vec(tok_type, lit),
-            None => self.add_token_in_vec(tok_type, Box::<Option<i8>>::new(None)),
-        }
-    }
-
-    fn advance(&mut self) -> char {
-        let local_current = self.current;
-        self.current += 1;
-        self.source_iter[local_current]
-    }
-
-    fn lex_number(&mut self) {
-        while self.char_is_digit(self.lookahead()) {
-            self.advance();
-        }
-        if self.lookahead() == '.' && self.char_is_digit(self.lookahead_2()) {
-            self.advance();
-            while self.char_is_digit(self.lookahead()) {
-                self.advance();
-            }
-        }
-        let literal = self.source[self.start..self.current]
-            .to_owned()
-            .parse::<f32>();
-        match literal {
-            Ok(n) => self.add_token(TokenType::NUMBER, Some(Box::new(literal))),
-            Err(_) => show_error(self.line, "Invalid number literal somehow".to_owned()),
-        }
-    }
-
-    fn lookahead(&self) -> char {
-        if self.is_at_end() {
-            return '\0';
-        }
-        self.source_iter[self.current]
-    }
-
-    fn lookahead_2(&self) -> char {
-        if self.current + 1 >= self.source_iter.len() {
-            return '\0';
-        }
-        self.source_iter[self.current + 1]
-    }
-
-    fn add_token_in_vec(&mut self, token_type: TokenType, literal: Box<dyn std::any::Any>) {
-        let substr = self.source[self.start..self.current].to_owned();
-        self.tokens
-            .push(Token::new(token_type, substr, literal, self.line));
-    }
-
-    fn char_is_digit(&self, c: char) -> bool {
-        c >= '0' && c <= '9'
-    }
-
-    fn check_curr(&mut self, expected: char) -> bool {
-        if self.lookahead() == expected {
-            self.advance();
-            return true;
-        }
-        false
-    }
-
-    fn lex_string(&mut self) {
-        while !self.is_at_end() && self.lookahead() != '"' {
-            if self.lookahead() == '\n' {
-                self.line += 1;
-            }
-            self.advance();
-        }
-        if self.is_at_end() {
-            show_error(self.line, "Unterminated string".to_owned());
-        }
-        self.advance();
-
-        let value = self.source[self.start + 1..self.current - 1].to_owned();
-        self.add_token(TokenType::STRING, Some(Box::new(value)));
     }
 }
